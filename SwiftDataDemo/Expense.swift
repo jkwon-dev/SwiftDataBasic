0692f2c92a16fe2543c2b1933bb7d4ea4f3ae888
jkwon-dev <kwonejk@gmail.com>
2023-11-28 17:27:19 +0900

    commit

---
+++ SwiftDataDemo/Expense.swift
@@ -0,0 +1,22 @@
+//
+//  Expense.swift
+//  SwiftDataDemo
+//
+//  Created by kwon eunji on 11/28/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Expense {
+    var name: String
+    var date: Date
+    var value: Double
+    
+    init(name: String, date: Date, value: Double) {
+        self.name = name
+        self.date = date
+        self.value = value
+    }
+}
